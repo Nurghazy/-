b98ccba25f0d35ba8f983c8d77d7d3a6239023b2
Nurghazy <100779461+Nurghazy@users.noreply.github.com>
2022-04-02 14:33:14 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*=========*/
 $a=2;
 $b=4;
-if($a>2 && $a<11)
+if($a>2 && $a<11 && $b>6 && $b<14)
 {
     echo "True ";
 }
@@ -11,21 +11,17 @@ else
      echo "False ";
     }
 
-if($b>6 && $b<14)
-{
-    echo "True ";
-}
-else
-{
-     echo "False ";
-    }
 
 echo $a+$b, "<br>";
 /*=========*/
-$month = 6;
+$month = 23;
 if($month <= 2 || $month == 12)
 {
 	echo "Zima" , "<br>";
+}
+else
+{
+	echo "error","<br>";
 }
 if($month <= 5 && $month >=3 )
 {
@@ -42,6 +38,6 @@ if($month <= 11 && $month >=9 )
 /*======*/
 echo "halo", "<br>";
 /*======*/
-$arr = [2, 5, 3, 9];
-$result = $arr[0] * $arr[1] + $arr[2] * $arr[3];
+$arr = [24, 47, 6, 13, 5, 89];
+$result = ($arr[6] - $arr[2]) + ($arr[1] - $arr[4]) + ($arr[2] - $arr[3]);
 echo $result;
