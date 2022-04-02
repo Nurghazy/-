ca42f216413ad4fc7d9116fe4c469ca92d505117
Nurghazy <100779461+Nurghazy@users.noreply.github.com>
2022-04-02 14:54:35 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 /*=========*/
 $a=2;
-$b=4;
-if($a>2 && $a<11 && $b>6 && $b<14)
+$b=7;
+if($a>2 && $a<11 || $b>6 && $b<14)
 {
     echo "True ";
 }
@@ -14,26 +14,27 @@ else
 
 echo $a+$b, "<br>";
 /*=========*/
-$month = 23;
-if($month <= 2 || $month == 12)
+$month = -11;
+if($month <= 2 && $month == 12 && $month>0)
 {
 	echo "Zima" , "<br>";
+}
+
+elseif($month <= 5 && $month >=3 )
+{
+	echo "Vesna" , "<br>";
+}
+elseif($month <= 8 && $month >=6 )
+{
+	echo "Leto" , "<br>";
+}
+elseif($month <= 11 && $month >=9 )
+{
+	echo "Osen" , "<br>";
 }
 else
 {
 	echo "error","<br>";
-}
-if($month <= 5 && $month >=3 )
-{
-	echo "Vesna" , "<br>";
-}
-if($month <= 8 && $month >=6 )
-{
-	echo "Leto" , "<br>";
-}
-if($month <= 11 && $month >=9 )
-{
-	echo "Osen" , "<br>";
 }
 /*======*/
 echo "halo", "<br>";
