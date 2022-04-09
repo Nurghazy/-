dde02177279c496c3bcc7001e11f407cb62a2ee2
Nurghazy <100779461+Nurghazy@users.noreply.github.com>
2022-04-09 08:32:38 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,44 +1,23 @@
 <?php
-/*=========*/
-$a=2;
-$b=7;
-if($a>2 && $a<11 || $b>6 && $b<14)
-{
-    echo "True ";
-}
-else
-{
-     echo "False ";
-    }
+    $a=70;
+    $b=35;
+    $c = $a/$b;
+    echo "70/35=" , $c, "\r\n";
+    $d = 10;
+    $num = $c*$d;
+    echo "10*2=", $num;
+?>
+<br>
 
+<?php
+    $age = 18;
+    echo "Мне ", $age, "лет";
+?>
+ <br>
 
-echo $a+$b, "<br>";
-/*=========*/
-$month = -11;
-if($month <= 2 && $month == 12 && $month>0)
-{
-	echo "Zima" , "<br>";
-}
+<?php
+$str = substr("Ermakhan",0, 3);
 
-elseif($month <= 5 && $month >=3 )
-{
-	echo "Vesna" , "<br>";
-}
-elseif($month <= 8 && $month >=6 )
-{
-	echo "Leto" , "<br>";
-}
-elseif($month <= 11 && $month >=9 )
-{
-	echo "Osen" , "<br>";
-}
-else
-{
-	echo "error","<br>";
-}
-/*======*/
-echo "halo", "<br>";
-/*======*/
-$arr = [24, 47, 6, 13, 5, 89];
-$result = ($arr[6] - $arr[2]) + ($arr[1] - $arr[4]) + ($arr[2] - $arr[3]);
-echo $result;
+echo "$str"
+?>
+
